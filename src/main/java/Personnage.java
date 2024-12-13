f322632ecc44abd7a8b7ba512046d48eb6245e89
mehdi <faddak.mehdi@gmail.com>
2024-12-13 10:26:02 +0100

    refactored 7 tests completed, 6 failed

--- src/main/java/Personnage.java
+++ src/main/java/Personnage.java
@@ -1,13 +1,15 @@
 public class Personnage {
+    private static final String[] ORIENTATIONS = {"NORD", "EST", "SUD", "OUEST"};
+    private int orientationIndex;
     public Personnage() {
-        
+        this.orientationIndex = 0;
     }
 
     
     
     public String tourner(int fois) {
-        System.out.println("EST\nNorth\nSUD\nWEST");
-        return "";
+        orientationIndex = (orientationIndex + 4*fois) % ORIENTATIONS.length;
+        return ORIENTATIONS[orientationIndex];
     }
 
     
